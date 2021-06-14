7ab5e7740aa01916f73612ef293657d8317f7e2e
yeoreumy <549747168@qq.com>
2021-06-14 16:37:15 +0800

    e2e test

--- cypress/integration/pokermon.js
+++ cypress/integration/pokermon.js
@@ -6,10 +6,10 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
-  // it('can open subpage', function () {
-  //   cy.visit('http://localhost:5000')
-  //   cy.contains('ivysaur')
-  //     .click()
-  //     .contains('chlorophyll')
-  // })
+  it('can open subpage', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+      .click()
+      .contains('chlorophyll')
+  })
 })
