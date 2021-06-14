db744fd9be33eeb11fb43f1bcf813d6858b099a0
yeoreumy <549747168@qq.com>
2021-06-14 16:29:06 +0800

    lint

--- cypress/integration/pokermon.spec.js
+++ cypress/integration/pokermon.spec.js
@@ -1,11 +1,11 @@
 describe('Pokedex', function () {
-  it("front page can be opened", function () {
-    cy.visit("http://localhost:5000");
-    cy.contains("ivysaur");
+  it('front page can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
     cy.contains(
-      "Pokémon and Pokémon character names are trademarks of Nintendo."
-    );
-  });
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  })
   // it('can open subpage', function () {
   //   cy.visit('http://localhost:5000')
   //   cy.contains('ivysaur')
